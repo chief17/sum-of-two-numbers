9c0e93c80eb137d1c34fef405cae5e443129fd5f
Nihit <johari.nihit@gmail.com>
2023-09-04 22:16:35 +0530

    First Commit includes complete code written in one session

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("Enter First Number: ");
+	int first=0;
+	scanf("%d",&first);
+	printf("Enter Second Number:");
+	int second=0;
+	scanf("%d",&second);
+	printf("The sum of the two numbers is %d\n",first+second);
+	return 0;
+}
